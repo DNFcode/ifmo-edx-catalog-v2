75b1812c9d6e24f031b4ed5f6225ff29a4176551
Nikita Vasiliev <dnfletter@gmail.com>
2015-11-18 13:45:35 +0300

    None Subject fix

--- ifmo_catalog/models.py
+++ ifmo_catalog/models.py
@@ -5,7 +5,13 @@ from xmodule_django.models import CourseKeyField
 class Subject(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class SubjectCourses(models.Model):
     subject = models.ForeignKey(Subject)
     course_id = CourseKeyField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return "%s %s" % (self.subject.name, self.course_id)
--- ifmo_catalog/utils.py
+++ ifmo_catalog/utils.py
@@ -1,9 +1,12 @@
 from ifmo_catalog.models import *
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def get_course_subject(course):
-    subject = SubjectCourses.objects.get(course_id=course.id).subject
-    return subject.name
+    try:
+        subject = SubjectCourses.objects.get(course_id=course.id).subject
+        return subject.name
+    except ObjectDoesNotExist:
+        return "None"
 
 
 def get_all_subjects():

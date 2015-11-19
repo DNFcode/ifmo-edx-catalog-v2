2cb5682d3c5875e277d39e560b0ef565b33e9329
Nikita <dnfletter@gmail.com>
2015-11-19 15:49:42 +0300

    Admin search added

--- ifmo_catalog/admin.py
+++ ifmo_catalog/admin.py
@@ -4,6 +4,8 @@ from ifmo_catalog.models import *
 
 class SubjectCoursesAdmin(admin.ModelAdmin):
     list_filter = ('subject',)
+    search_fields = ['course_id']
+
 
 admin.site.register(Subject)
 admin.site.register(SubjectCourses, SubjectCoursesAdmin)
--- ifmo_catalog/utils.py
+++ ifmo_catalog/utils.py
@@ -1,4 +1,5 @@
 from ifmo_catalog.models import *
+from lms.djangoapps.courseware.courses import get_courses
 
 
 def get_course_subjects(course):
@@ -11,3 +12,11 @@ def get_all_subjects():
     subjects = Subject.objects.filter(parent=None)
     subjects = [s for s in subjects if s.subject_set]
     return subjects
+
+
+def get_courses_availability(user):
+    courses = get_courses(user)
+    return reduce(lambda (up, curr), course: (
+        up or not course.has_started(),  # upcoming courses
+        curr or (course.has_started() and not course.has_ended())  # current courses
+    ), courses, (False, False))
